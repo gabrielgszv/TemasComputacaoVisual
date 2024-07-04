c5ac3a164e91cf64adae584996d42831f505196c
gabrielgszv <145291590+gabrielgszv@users.noreply.github.com>
2024-07-04 16:50:21 -0300

    Create polygonsprint.py

---
+++ PROG1/Lista 8/polygonsprint.py
@@ -0,0 +1,133 @@
+import turtle
+import re
+
+class Point2D:
+    def __init__(self, x, y):
+        self.coord = (x, y)
+
+    def __str__(self):
+        return f'{self.coord}'
+
+class Polygon:
+    def __init__(self, points, color):
+        self.points = points
+        self.color = color
+
+    def __str__(self):
+        pontos = []
+        for i in self.points:
+            pontos.append(i.coord)
+        pontos = str(pontos)    
+        return f'Pontos: {pontos[1:-1]}\nCor: {self.color}'
+
+class Polygons:
+    def __init__(self):
+        self.polygons = {}   
+  
+    def add_polygon(self, polygon: Polygon, name: str):    
+        self.polygons[name] = polygon
+
+    def remove_polygon(self, name: str):
+        self.polygons.pop(name)
+
+    def save_to_file(self, filename: str):
+        with open(filename, 'w') as f:
+            for p in self.polygons:
+                for i in self.polygons[p].points:
+                    f.write(str(i.coord))
+                f.write(f'; {self.polygons[p].color}; {p}\n')
+
+    def load_from_file(self, filename: str):
+        with open(filename, 'r') as f:
+            padrao = r'\((-?\d+),\s*(-?\d+)\)'
+            i = 0
+            for linha in f:
+                #em cada linha criar um objeto do tipo polygon
+                pontospoligono = []
+                #receber os pontos
+                partes = linha.split('; ')
+                cor = partes[1]
+                nome = partes[2]
+                if partes[2][-1] == '\n':
+                    nome = partes[2][0:-1]
+                pontos = re.findall(padrao, partes[0])
+                j = 0
+                p = []
+                for tupla in pontos:
+                    for i in tupla:
+                        p.append(int(i))
+                while j < len(pontos) :
+                    point = Point2D(p[j], p[j+1])
+                    pontospoligono.append(point)
+                    j += 1   
+                poligono = Polygon(pontospoligono, cor)
+                self.add_polygon(poligono, nome)  
+
+                    
+
+
+
+#5 Questao
+
+def plot_polygons(poligono: Polygons):
+    screen = turtle.Screen()
+
+    t = turtle.Turtle()
+    poligonos = poligono.polygons
+    p = []
+    for k in poligonos:
+        p = poligonos[k].points
+        t.penup()
+        t.goto(p[0].coord)
+        t.pendown()
+
+        for i in p:
+            t.goto(i.coord)
+
+        t.goto(p[0].coord) 
+        cor = poligonos[k].color
+        t.fillcolor(cor)
+        t.begin_fill()
+        for i in p:
+            t.goto(i.coord)
+        t.end_fill()
+        
+    screen.exitonclick()      
+
+
+
+
+if __name__ == '__main__':
+
+    poligonos = Polygons()
+
+    p1 = Point2D(0,0)
+    print(p1)
+    p2 = Point2D(100,1)
+    p3 = Point2D(0,245)
+    p4 = Point2D(-100,214)
+
+    poligono = Polygon([p1,p2,p3,p4],'green')
+    print(poligono)
+    poligonos.add_polygon(poligono,'primeiro')
+
+    p1 = Point2D(140,-65)
+    p2 = Point2D(33,-21)
+    p3 = Point2D(12,-92)
+
+    poligono = Polygon([p1,p2,p3],'red')
+    poligonos.add_polygon(poligono,'segundo')
+
+    p1 = Point2D(-123,-234)
+    p2 = Point2D(24,365)
+    p3 = Point2D(-310,92)
+
+    poligono = Polygon([p1,p2,p3],'blue')
+    poligonos.add_polygon(poligono,'terceiro')
+    
+    poligonos.save_to_file('texto.txt')
+    poligonos.load_from_file('puxar.txt')
+    poligonos.save_to_file('texto.txt')
+
+    print(plot_polygons(poligonos))
+    
