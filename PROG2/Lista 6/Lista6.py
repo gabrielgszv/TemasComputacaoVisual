f1e59a42261f24cde41b3a8181830a106183e3ef
gabrielgszv <145291590+gabrielgszv@users.noreply.github.com>
2024-11-07 15:38:29 -0300

    Update Lista6.py

--- PROG2/Lista 6/Lista6.py
+++ PROG2/Lista 6/Lista6.py
@@ -4,6 +4,64 @@
 
 #---------------------------------------------------------------------------------------
 
+'''
+Classe grafo da lista anterior até a linha 60
+'''
+
+class Grafo:
+    def __init__(self):
+        self.grafo = {}
+
+    def adjacent(self, x, y):
+        if x in self.grafo:
+            if y in self.grafo[x]['Vizinhos']:
+                return True
+        return False    
+
+    def neighbors(self, x) -> list:
+        return list(self.grafo[x]['Vizinhos'])
+
+    def add_vertex(self, x):
+        if x in self.grafo:
+            return False
+        else:
+            self.grafo[x] = {'Vizinhos': []}
+            return True
+        
+    def remove_vertex(self, x):
+        if x in self.grafo:
+            self.grafo.pop(x)
+            return True    
+        else:
+            return False
+
+    def add_edge(self, x, y):
+        if y in self.grafo[x]:
+            return False
+        self.grafo[x]['Vizinhos'].append(y)
+        return True
+        
+    def remove_edge(self, x, y):
+        if y in self.grafo[x]:
+            self.grafo[x].pop(y)
+            return True
+        return False
+    
+    def get_vertex_value(self, x):
+        if x in self.grafo:
+            if 'Valor' in self.grafo[x]:
+                return self.grafo[x]['Valor']
+            else:
+                return None
+
+    def set_vertex_value(self, x, v):
+        if x in self.grafo.keys():
+            self.grafo[x]['Valor'] = v    
+
+'''
+Função para achar o caminho
+'''       
+
 def caminho(grafo, ponto):
     visitados = []
     fila = [ponto]
@@ -18,14 +76,33 @@ def caminho(grafo, ponto):
     print(visitados)            
 
 grafo = {
-    0: [1, 2],
-    1: [0, 3, 4],
-    2: [0],
-    3: [1],
-    4: [1]
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A'],
+    'D': ['B'],
+    'E': ['B']
 }
 
-caminho(grafo, 2)
+grafo1 = Grafo()
+grafo1.add_vertex('A')
+grafo1.add_vertex('B')
+grafo1.add_vertex('C')
+grafo1.add_vertex('D')
+grafo1.add_vertex('E')
+grafo1.add_edge('A','B')
+grafo1.add_edge('A','C')
+grafo1.add_edge('B','A')
+grafo1.add_edge('B','D')
+grafo1.add_edge('B','E')
+grafo1.add_edge('C','A')
+grafo1.add_edge('D','B')
+grafo1.add_edge('E','B')
+
+print(grafo1.grafo)
+
+
+
+caminho(grafo, 'D')
 
 #---------------------------------------------------------------------------------------
 
@@ -65,12 +142,12 @@ def numero_de_ilhas(txt):
             def ilhar(m, n):
                 if (m,n) not in visitados:
                     visitados.append((m, n))
-                    if (m >= 0 and n >= 0) and (m < len(matrix) and n < len(matrix[0])):
+                    if 0 <= m < len(matrix) and 0 <= n < len(matrix[0]):
                         if matrix[m][n] == '1':
                             for p, q in direcoes:
-                                if (m+p >= 0 and n+q >= 0) and (m+p < len(matrix) and n+q < len(matrix[0])):
+                                if 0 <= m+p < len(matrix) and 0 <= n+q < len(matrix[0]):
                                     ilhar(m+p, n+q)   
-            if (x >= 0 and y >= 0) and (x < len(matrix) and y < len(matrix[0])):                                
+            if 0 <= x < len(matrix) and 0 <= y < len(matrix[0]):                               
                 if matrix[x][y] == '1' and (x,y) not in visitados:
                     qilha +=1
 
@@ -108,33 +185,35 @@ def numero_de_ilhas_modificado(txt):
             def ilhar(m, n):
                 if (m,n) not in visitados:
                     visitados.append((m, n))
-                    if matrix[m][n] == '1':
-                        ilha.append((m, n))
-                        for p, q in direcoes:
-                            if (m+p >= 0 and n+q >= 0) and (m+p < len(matrix[0]) and n+q < len(matrix)):
-                                ilhar(m+p, n+q)   
+                    if 0 <= m < len(matrix) and 0 <= n < len(matrix[0]):
+                        if matrix[m][n] == '1':
+                            ilha.append((m, n))
+                            for p, q in direcoes:
+                                if 0 <= m+p < len(matrix) and 0 <= n+p < len(matrix[0]):
+                                    ilhar(m+p, n+q)   
      
             ilhas.append(ilha)                          
-
-            if matrix[x][y] == '1' and (x,y) not in visitados:
-                qilha +=1
+            if 0 <= x < len(matrix) and 0 <= y < len(matrix[0]):
+                if matrix[x][y] == '1' and (x,y) not in visitados:
+                    qilha +=1
 
             ilhar(x,y)
 
     ilhasf = []
-
     for ilha in ilhas:
         if len(ilha) > 0:
             ilhasf.append(ilha)
 
     menor_ilha = min(ilhasf, key=len)      
     maior_ilha = max(ilhasf, key=len)
+    print('menor',menor_ilha)
+    print('maior',maior_ilha)
 
     return qilha         
 
 
 
-print(numero_de_ilhas('test_map.txt'))
+print(numero_de_ilhas_modificado('test_map.txt'))
 
 
 #---------------------------------------------------------------------------------------
