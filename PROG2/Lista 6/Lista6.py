dfa371b9f5e51943bd5710d3274a0100a1355af5
gabrielgszv <145291590+gabrielgszv@users.noreply.github.com>
2024-11-08 09:13:57 -0300

    Update Lista6.py

--- PROG2/Lista 6/Lista6.py
+++ PROG2/Lista 6/Lista6.py
@@ -5,7 +5,7 @@
 #---------------------------------------------------------------------------------------
 
 '''
-Classe grafo da lista anterior até a linha 60
+Até a linha 60 é a mesma classe grafo da lista anterior para facilitar
 '''
 
 class Grafo:
@@ -25,7 +25,7 @@ class Grafo:
         if x in self.grafo:
             return False
         else:
-            self.grafo[x] = {'Vizinhos': []}
+            self.grafo[x] = {'Vizinhos': [],'Valor': None}
             return True
         
     def remove_vertex(self, x):
@@ -58,31 +58,27 @@ class Grafo:
         if x in self.grafo.keys():
             self.grafo[x]['Valor'] = v    
 
-'''
-Função para achar o caminho
-'''       
+#Função da questão     
 
-def caminho(grafo, ponto):
+def bfs(grafo, v_inicial = None):
+
+    if v_inicial is None:
+        v_inicial = list(grafo.grafo.keys())[0]
+
     visitados = []
-    fila = [ponto]
+    fila = [v_inicial]
 
     while fila:
         vertice = fila.pop(0)
         visitados.append(vertice)
 
-        for k in grafo[vertice]:
+        for k in grafo.grafo[vertice]['Vizinhos']:
             if k not in visitados:
                 fila.append(k)
-    print(visitados)            
+    return visitados           
 
-grafo = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A'],
-    'D': ['B'],
-    'E': ['B']
-}
-
+#Exemplo para teste
+'''
 grafo1 = Grafo()
 grafo1.add_vertex('A')
 grafo1.add_vertex('B')
@@ -98,14 +94,48 @@ grafo1.add_edge('C','A')
 grafo1.add_edge('D','B')
 grafo1.add_edge('E','B')
 
+bfs(grafo1, 'D')
+'''
+#---------------------------------------------------------------------------------------
 
-caminho(grafo, 'D')
+#Questão 2
 
+#---------------------------------------------------------------------------------------
+
+'''
+Aproveitando da função da questão passada para ter o caminho,
+agora é so percorrer o caminho e achar qual vertice tem essa propriedade,
+caso nao tenha a função retorna None
+'''
+
+def busca_propriedade(G, value):
+
+    caminho = bfs(G)
+
+    for vertice in caminho:
+        if G.grafo[vertice]['Valor'] == value:
+            return vertice
+        
+    return None    
+
+#Exemplo para teste
+'''
+grafo1.set_vertex_value('A', 10)
+grafo1.set_vertex_value('B', 20)
+grafo1.set_vertex_value('C', 30)
+grafo1.set_vertex_value('D', 40)
+grafo1.set_vertex_value('E', 50)
+print('Vértice',busca_propriedade(grafo1, 30))
+'''
 #---------------------------------------------------------------------------------------
 
 #Questão 3
 
 #---------------------------------------------------------------------------------------
+
+'''
+Função apenas para transformar o txt em uma matriz
+'''
 
 def txt_para_matriz(matriz):
 
@@ -122,15 +152,13 @@ def txt_para_matriz(matriz):
 
 def numero_de_ilhas(txt):
 
-    #Transformar em uma matriz novamente
-
     matrix = txt_para_matriz(txt)    
-
-    #Agora passando por todos os elementos da matriz par acontar as ilhas
 
     visitados = []
     direcoes = [(0, -1), (1,-1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
     qilha = 0
+
+    #Função para percorrer uma ilha inteira
 
     def ilhar(m, n):
         if (m,n) not in visitados:
@@ -143,16 +171,19 @@ def numero_de_ilhas(txt):
                         if 0 <= m+p < len(matrix) and 0 <= n+q < len(matrix[0]):
                             ilhar(m+p, n+q)  
 
+    #Percorrer todos os elementos da matriz                        
+
     for x in range(len(matrix)):
         for y in range(len(matrix[0])):
+
             if 0 <= x < len(matrix) and 0 <= y < len(matrix[0]):                              
                 if matrix[x][y] == '1' and (x,y) not in visitados:
                     qilha +=1
+
             ilhar(x,y)  
-            
+
     return qilha               
     
-print(numero_de_ilhas('test.txt'))
 
 #---------------------------------------------------------------------------------------
 
@@ -162,9 +193,8 @@ print(numero_de_ilhas('test.txt'))
    
 '''
 Mesmo codigo que a questao anterior so que com as modificações para retornar
-as coordenadas do centroide da maior e menor ir
+as coordenadas do centroide da maior e menor ilha
 '''
-
 
 def numero_de_ilhas_modificado(txt):
 
@@ -173,7 +203,11 @@ def numero_de_ilhas_modificado(txt):
     visitados = []
     direcoes = [(0, -1), (1,-1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
     qilha = 0
+
     ilhas = []
+
+    #Função para percorrer uma ilha inteira
+
     def ilhar(m, n):
         if (m,n) not in visitados:
             visitados.append((m, n))
@@ -184,32 +218,63 @@ def numero_de_ilhas_modificado(txt):
 
                     for p, q in direcoes:
                         if 0 <= m+p < len(matrix) and 0 <= n+p < len(matrix[0]):
-                            ilhar(m+p, n+q) 
+                            ilhar(m+p, n+q)
+
+    #Percorrer todos os elementos da matriz                           
 
     for x in range(len(matrix)):
         for y in range(len(matrix[0])):
-            ilha = []
 
-            ilhas.append(ilha)                          
+            #Sempre que achar uma ilha, vai salvar o caminho inteiro dessa ilha na lista ilhas
+
+            ilha = []
+            ilhas.append(ilha)  
+                        
             if 0 <= x < len(matrix) and 0 <= y < len(matrix[0]):
                 if matrix[x][y] == '1' and (x,y) not in visitados:
                     qilha +=1
 
             ilhar(x,y)
 
+    #Esse for é apenas para tirar as listas nulas 
+
     ilhasf = []
     for ilha in ilhas:
         if len(ilha) > 0:
             ilhasf.append(ilha)
 
+    #Ver qual a menor e a maior ilha        
+
     menor_ilha = min(ilhasf, key=len)      
     maior_ilha = max(ilhasf, key=len)
-    print('menor',menor_ilha)
-    print('maior',maior_ilha)
 
-    return qilha         
+    #Calcular o centroide
 
-print(numero_de_ilhas_modificado('test.txt'))
+    #Menor
+    x, y = 0, 0
+
+    for p, q in menor_ilha:
+        x += p
+        y += q
+
+    x = x/len(menor_ilha)   
+    y = y/len(menor_ilha) 
+
+    centroide_menor = (x, y)
+    
+    #Maior
+    x, y = 0, 0
+
+    for p, q in maior_ilha:
+        x += p
+        y += q
+
+    x = x/len(maior_ilha)   
+    y = y/len(maior_ilha) 
+
+    centroide_maior = (x, y)
+
+    return centroide_menor, centroide_maior   
 
 
 #---------------------------------------------------------------------------------------
@@ -217,9 +282,21 @@ print(numero_de_ilhas_modificado('test.txt'))
 #Questão 5
 
 #---------------------------------------------------------------------------------------
-def ilha(txt):
+'''
+A função vai passar por cada elemento da matriz e olhar nas quatro direções
+para ver se tem terra, só sera considerado ilha se tiver exatamente nas quatro direções,
+não será considerado por exemplo um lago que esteja na primeira linha e tenha terra na
+esquerda, direita e abaixo(o que deu a entender da questão)
 
-    matrix = txt_para_matriz(txt)   
+E não sei se era pra retornar apenas se a matriz tinha lago ou nao,
+então coloquei para retornar todos os lagos encontrados(caso exista)
+'''
+
+
+def lago(txt):
+
+    matrix = txt_para_matriz(txt)  
+
     direcoes = [(0, -1), (1, 0), (0, 1), (-1, 0)]
     ilhas = []
 
@@ -240,5 +317,3 @@ def ilha(txt):
         return ilhas
     else:
         return None            
-
-print(ilha('test_map.txt'))
